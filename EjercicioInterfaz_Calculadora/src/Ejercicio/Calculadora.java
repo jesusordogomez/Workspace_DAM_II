17c9bdb4c76ad60c2f0a44217f7a962818842099
Jesus Ordoñez <jesus.gomez1992@gmail.com>
2023-09-28 20:40:13 +0200

    siguiente paso calculadora

--- EjercicioInterfaz_Calculadora/src/Ejercicio/Calculadora.java
+++ EjercicioInterfaz_Calculadora/src/Ejercicio/Calculadora.java
@@ -6,6 +6,8 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import javax.swing.JTextField;
@@ -51,109 +53,217 @@ public class Calculadora {
 		frmCalculadora.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmCalculadora.getContentPane().setLayout(null);
 		
+		
+		//creo el textArea
 		JTextArea textArea = new JTextArea();
-		textArea.setBounds(33, 47, 260, 22);
+		textArea.setBounds(30, 81, 196, 22);
 		frmCalculadora.getContentPane().add(textArea);
 		
-		JButton btnNewButton = new JButton("7");
-		btnNewButton.setBounds(33, 114, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("8");
-		btnNewButton_1.setBounds(104, 114, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_1);
+		//pulsar teclado
+		textArea.addKeyListener(new KeyAdapter() {
+		    @Override
+		    public void keyTyped(KeyEvent e) {
+		        char c = e.getKeyChar();
+		        if (!Character.isDigit(c) && c != '+' && c != '-' && c != '*' && c != '/' && c != '=' && c!=',') {
+		            e.consume(); // Evitar que se escriba el carácter no permitido
+		        }
+		    }
+		});
 		
-		JButton btnNewButton_2 = new JButton("9");
-		btnNewButton_2.setBounds(178, 114, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_2);
 		
-		JButton btnNewButton_3 = new JButton("4");
-		btnNewButton_3.setBounds(33, 148, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_3);
-		
-		JButton btnNewButton_3_1 = new JButton("5");
-		btnNewButton_3_1.setBounds(104, 148, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_3_1);
-		
-		JButton btnNewButton_3_2 = new JButton("6");
-		btnNewButton_3_2.setBounds(178, 148, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_3_2);
-		
-		JButton btnNewButton_3_3 = new JButton("1");
-		btnNewButton_3_3.addActionListener(new ActionListener() {
+		//creo el boton7
+		JButton boton7 = new JButton("7");
+		boton7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textArea.append("7");
+				textArea.requestFocusInWindow();
 			}
 		});
-		btnNewButton_3_3.setBounds(33, 182, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_3_3);
+		boton7.setBounds(33, 114, 48, 23);
+		frmCalculadora.getContentPane().add(boton7);
 		
-		JButton btnNewButton_3_4 = new JButton("2");
-		btnNewButton_3_4.setBounds(104, 182, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_3_4);
 		
-		JButton btnNewButton_3_5 = new JButton("3");
-		btnNewButton_3_5.setBounds(178, 182, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_3_5);
-		
-		JButton btnNewButton_3_6 = new JButton("boton");
-		btnNewButton_3_6.addActionListener(new ActionListener() {
+		//creo el boton 8
+		JButton boton8 = new JButton("8");
+		boton8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textArea.append("8");
+				textArea.requestFocusInWindow();
 			}
 		});
-		btnNewButton_3_6.setBounds(33, 216, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_3_6);
+		boton8.setBounds(104, 114, 48, 23);
+		frmCalculadora.getContentPane().add(boton8);
 		
-		JButton btnNewButton_3_7 = new JButton("0");
-		btnNewButton_3_7.addActionListener(new ActionListener() {
+		//creo el boton 9
+		JButton boton9 = new JButton("9");
+		boton9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textArea.append("9");
+				textArea.requestFocusInWindow();
 			}
 		});
-		btnNewButton_3_7.setBounds(104, 216, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_3_7);
+		boton9.setBounds(178, 114, 48, 23);
+		frmCalculadora.getContentPane().add(boton9);
 		
-		JButton btnNewButton_3_8 = new JButton(",");
-		btnNewButton_3_8.setBounds(178, 216, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_3_8);
+		//creo el boton 4
+		JButton boton4 = new JButton("4");
+		boton4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("4");
+				textArea.requestFocusInWindow();
+			}
+		});
+		boton4.setBounds(33, 148, 48, 23);
+		frmCalculadora.getContentPane().add(boton4);
 		
-		JButton btnNewButton_3_9 = new JButton("x");
-		btnNewButton_3_9.setBounds(245, 114, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_3_9);
+		//creo el boton 5
+		JButton boton5 = new JButton("5");
+		boton5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("5");
+				textArea.requestFocusInWindow();
+			}
+		});
+		boton5.setBounds(104, 148, 48, 23);
+		frmCalculadora.getContentPane().add(boton5);
 		
-		JButton btnNewButton_3_10 = new JButton("-");
-		btnNewButton_3_10.setBounds(245, 148, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_3_10);
+		//creo el boton 6
+		JButton boton6 = new JButton("6");
+		boton6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("6");
+				textArea.requestFocusInWindow();
+			}
+		});
+		boton6.setBounds(178, 148, 48, 23);
+		frmCalculadora.getContentPane().add(boton6);
 		
-		JButton btnNewButton_3_11 = new JButton("+");
-		btnNewButton_3_11.setBounds(245, 182, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_3_11);
 		
-		JButton btnNewButton_3_12 = new JButton("=");
-		btnNewButton_3_12.setBounds(245, 216, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_3_12);
+		//creo el boton 1
+		JButton boton1 = new JButton("1");
+		boton1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("1");
+				textArea.requestFocusInWindow();
+			}
+		});
+		boton1.setBounds(33, 182, 48, 23);
+		frmCalculadora.getContentPane().add(boton1);
 		
-		JButton btnBoton = new JButton("boton");
-		btnBoton.setBounds(33, 80, 48, 23);
-		frmCalculadora.getContentPane().add(btnBoton);
+		//creo el boton 2
+		JButton boton2 = new JButton("2");
+		boton2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("2");
+				textArea.requestFocusInWindow();
+			}
+		});
+		boton2.setBounds(104, 182, 48, 23);
+		frmCalculadora.getContentPane().add(boton2);
 		
-		JButton btnX = new JButton("xº");
-		btnX.setBounds(104, 80, 48, 23);
-		frmCalculadora.getContentPane().add(btnX);
+		//creo el boton 3
+		JButton boton3 = new JButton("3");
+		boton3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("3");
+				textArea.requestFocusInWindow();
+			}
+		});
+		boton3.setBounds(178, 182, 48, 23);
+		frmCalculadora.getContentPane().add(boton3);
 		
-		JButton btnNewButton_6 = new JButton("/");
-		btnNewButton_6.setBounds(178, 80, 48, 23);
-		frmCalculadora.getContentPane().add(btnNewButton_6);
 		
-		JButton btnCe = new JButton("CE");
-		btnCe.setBounds(245, 80, 48, 23);
-		frmCalculadora.getContentPane().add(btnCe);
+		//creo el boton0
+		JButton boton0 = new JButton("0");
+		boton0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("0");
+				textArea.requestFocusInWindow();
+			}
+		});
+		boton0.setBounds(33, 216, 48, 23);
+		frmCalculadora.getContentPane().add(boton0);
+		
+		JButton coma = new JButton(",");
+		coma.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.append(",");
+				textArea.requestFocusInWindow();
+			}
+		});
+		coma.setBounds(104, 216, 48, 23);
+		frmCalculadora.getContentPane().add(coma);
+		
+		JButton botonx = new JButton("x");
+		botonx.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("*");
+				textArea.requestFocusInWindow();
+			}
+		});
+		botonx.setBounds(245, 148, 48, 23);
+		frmCalculadora.getContentPane().add(botonx);
+		
+		JButton resta = new JButton("-");
+		resta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("-");
+				textArea.requestFocusInWindow();
+			}
+		});
+		resta.setBounds(245, 182, 48, 23);
+		frmCalculadora.getContentPane().add(resta);
+		
+		JButton suma = new JButton("+");
+		suma.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("+");
+				textArea.requestFocusInWindow();
+			}
+		});
+		suma.setBounds(245, 216, 48, 23);
+		frmCalculadora.getContentPane().add(suma);
+		
+		JButton igual = new JButton("=");
+		igual.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("=");
+				textArea.requestFocusInWindow();
+			}
+		});
+		igual.setBounds(178, 216, 48, 23);
+		frmCalculadora.getContentPane().add(igual);
+		
+		JButton dividir = new JButton("/");
+		dividir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("/");
+				textArea.requestFocusInWindow();
+			}
+		});
+		dividir.setBounds(245, 114, 48, 23);
+		frmCalculadora.getContentPane().add(dividir);
+		
+		JButton limpia = new JButton("CE");
+		limpia.setFont(new Font("Tahoma", Font.PLAIN, 7));
+		limpia.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.setText("");
+				textArea.requestFocusInWindow();
+			}
+		});
+		limpia.setBounds(245, 80, 48, 23);
+		frmCalculadora.getContentPane().add(limpia);
 		
 		JTextArea txtrCalculadora = new JTextArea();
+		txtrCalculadora.setEnabled(false);
 		txtrCalculadora.setEditable(false);
-		txtrCalculadora.setFont(new Font("Sitka Heading", Font.BOLD, 21));
+		txtrCalculadora.setFont(new Font("Sitka Heading", Font.BOLD, 25));
 		txtrCalculadora.setForeground(new Color(0, 0, 0));
 		txtrCalculadora.setBackground(new Color(192, 192, 192));
 		txtrCalculadora.setText("CALCULADORA");
-		txtrCalculadora.setBounds(96, 11, 152, 22);
+		txtrCalculadora.setBounds(75, 27, 188, 43);
 		frmCalculadora.getContentPane().add(txtrCalculadora);
 	}
 }
