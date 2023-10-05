7d117bc2ed2013a1e298e4579d4acc388a2f0c1b
jesus <jesus.gomez1992@gmail.com>
2023-10-05 16:28:35 +0200

    cambios calculadora

--- EjercicioInterfaz_Calculadora/src/Ejercicio/Calculadora.java
+++ EjercicioInterfaz_Calculadora/src/Ejercicio/Calculadora.java
@@ -53,18 +53,26 @@ public class Calculadora {
 		frmCalculadora.getContentPane().setBackground(new Color(192, 192, 192));
 		frmCalculadora.setBounds(100, 100, 345, 275);
 		frmCalculadora.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmCalculadora.getContentPane().setLayout(null);
 
 
 		//creo el textArea
 		textfield = new JTextField();
-		textfield.setBounds(33, 37, 205, 31);
+		textfield.setBounds(33, 0, 205, 31);
 		Font fuente = new Font("Arial", Font.BOLD, 20);
+		frmCalculadora.getContentPane().setLayout(null);
 		textfield.setFont(fuente);
 		textfield.setHorizontalAlignment(JTextField.RIGHT);
 		frmCalculadora.getContentPane().add(textfield);
 
-
+		  textfield.addKeyListener(new KeyAdapter() {
+	            @Override
+	            public void keyPressed(KeyEvent e) {
+	                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+	                    calcularResultado();
+	                }
+	            }
+	        });
+		
 		//con este metodo solo puedo teclear los caracteres que yo necesito para la calculadora
 		textfield.addKeyListener(new KeyAdapter() {
 			@Override
@@ -79,128 +87,129 @@ public class Calculadora {
 
 		//creo el boton7
 		JButton boton7 = new JButton("7");
+		boton7.setBounds(33, 73, 61, 31);
 		boton7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textfield.setText(textfield.getText() + "7");
 				textfield.requestFocusInWindow();
 			}
 		});
-		boton7.setBounds(33, 73, 61, 31);
 		frmCalculadora.getContentPane().add(boton7);
 
 
 		//creo el boton 8
 		JButton boton8 = new JButton("8");
+		boton8.setBounds(107, 73, 64, 31);
 		boton8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textfield.setText(textfield.getText() + "8");
 				textfield.requestFocusInWindow();
 			}
 		});
-		boton8.setBounds(107, 73, 64, 31);
 		frmCalculadora.getContentPane().add(boton8);
 
 		//creo el boton 9
 		JButton boton9 = new JButton("9");
+		boton9.setBounds(181, 73, 57, 31);
 		boton9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textfield.setText(textfield.getText() + "9");
 				textfield.requestFocusInWindow();
 			}
 		});
-		boton9.setBounds(181, 73, 57, 31);
 		frmCalculadora.getContentPane().add(boton9);
 
 		//creo el boton 4
 		JButton boton4 = new JButton("4");
+		boton4.setBounds(33, 110, 61, 31);
 		boton4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textfield.setText(textfield.getText() + "4");
 				textfield.requestFocusInWindow();
 			}
 		});
-		boton4.setBounds(33, 110, 61, 31);
 		frmCalculadora.getContentPane().add(boton4);
 
 		//creo el boton 5
 		JButton boton5 = new JButton("5");
+		boton5.setBounds(107, 110, 64, 31);
 		boton5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textfield.setText(textfield.getText() + "5");
 				textfield.requestFocusInWindow();
 			}
 		});
-		boton5.setBounds(107, 110, 64, 31);
 		frmCalculadora.getContentPane().add(boton5);
 
 		//creo el boton 6
 		JButton boton6 = new JButton("6");
+		boton6.setBounds(181, 110, 57, 31);
 		boton6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textfield.setText(textfield.getText() + "6");
 				textfield.requestFocusInWindow();
 			}
 		});
-		boton6.setBounds(181, 110, 57, 31);
 		frmCalculadora.getContentPane().add(boton6);
 
 
 		//creo el boton 1
 		JButton boton1 = new JButton("1");
+		boton1.setBounds(33, 152, 61, 31);
 		boton1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textfield.setText(textfield.getText() + "1");
 				textfield.requestFocusInWindow();
 			}
 		});
-		boton1.setBounds(33, 152, 61, 31);
 		frmCalculadora.getContentPane().add(boton1);
 
 		//creo el boton 2
 		JButton boton2 = new JButton("2");
+		boton2.setBounds(107, 152, 64, 31);
 		boton2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textfield.setText(textfield.getText() + "2");
 				textfield.requestFocusInWindow();
 			}
 		});
-		boton2.setBounds(107, 152, 64, 31);
 		frmCalculadora.getContentPane().add(boton2);
 
 		//creo el boton 3
 		JButton boton3 = new JButton("3");
+		boton3.setBounds(181, 152, 57, 31);
 		boton3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textfield.setText(textfield.getText() + "3");
 				textfield.requestFocusInWindow();
 			}
 		});
-		boton3.setBounds(181, 152, 57, 31);
 		frmCalculadora.getContentPane().add(boton3);
 
 
 		//creo el boton0
 		JButton boton0 = new JButton("0");
+		boton0.setBounds(33, 194, 61, 31);
 		boton0.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textfield.setText(textfield.getText() + "0");
 				textfield.requestFocusInWindow();
 			}
 		});
-		boton0.setBounds(33, 194, 61, 31);
 		frmCalculadora.getContentPane().add(boton0);
 
 		JButton coma = new JButton(",");
+		coma.setBounds(107, 194, 64, 31);
 		coma.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textfield.setText(textfield.getText() + ",");
 				textfield.requestFocusInWindow();
 			}
 		});
-		coma.setBounds(107, 194, 64, 31);
 		frmCalculadora.getContentPane().add(coma);
 
 		JButton botonx = new JButton("x");
+		botonx.setBounds(248, 110, 61, 31);
 		botonx.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				recogerOperaciones = "*";
@@ -208,10 +217,10 @@ public class Calculadora {
 				textfield.requestFocusInWindow();
 			}
 		});
-		botonx.setBounds(248, 110, 61, 31);
 		frmCalculadora.getContentPane().add(botonx);
 
 		JButton resta = new JButton("-");
+		resta.setBounds(245, 152, 64, 31);
 		resta.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				recogerOperaciones = "-";
@@ -219,7 +228,6 @@ public class Calculadora {
 				textfield.requestFocusInWindow();
 			}
 		});
-		resta.setBounds(245, 152, 64, 31);
 		frmCalculadora.getContentPane().add(resta);
 
 		
@@ -253,6 +261,7 @@ public class Calculadora {
 		
 		
 		JButton limpia = new JButton("CE");
+		limpia.setBounds(248, 37, 61, 31);
 		limpia.setFont(new Font("Arial", Font.PLAIN, 7));
 		limpia.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -260,48 +269,134 @@ public class Calculadora {
 				textfield.requestFocusInWindow();
 			}
 		});
-		limpia.setBounds(248, 37, 61, 31);
 		frmCalculadora.getContentPane().add(limpia);
 		
-		igual = new JButton("=");
-		igual.setBounds(181, 194, 57, 31);
-		igual.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {
-		        String contenido = textfield.getText();
-		        String operador = "";
-		        double resultado = 0.0;
-
-		        if (contenido.contains("+")) {
-		            operador = "+";
-		        } else if (contenido.contains("-")) {
-		            operador = "-";
-		        } else if (contenido.contains("*")) {
-		            operador = "*";
-		        } else if (contenido.contains("/")) {
-		            operador = "/";
-		        }
-
-		        if (!operador.isEmpty()) {
-		            String[] calcularNumeros = contenido.split(Pattern.quote(operador));
-		            if (calcularNumeros.length == 2) {
-		                double num1 = Double.parseDouble(calcularNumeros[0]);
-		                double num2 = Double.parseDouble(calcularNumeros[1]);
-
-		                if (operador.equals("+")) resultado = num1 + num2;
-		                else if (operador.equals("-")) resultado = num1 - num2;
-		                else if (operador.equals("/")) resultado = num1 / num2;
-		                else if (operador.equals("*")) resultado = num1 * num2;
-		            }
-		        }
-
-		        textfield.setText(String.valueOf(resultado));
-		        textfield.requestFocusInWindow();
-		    }
-		});
-		frmCalculadora.getContentPane().add(igual);
+		
+		
+		 igual = new JButton("=");
+		 igual.setBounds(181, 194, 57, 31);
+	        igual.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+	                calcularResultado();
+	            }
+	        });
+	        frmCalculadora.getContentPane().add(igual);
+	    
+	
+	JButton botonPotencia = new JButton("^");
+	botonPotencia.setBounds(181, 36, 57, 31);
+	botonPotencia.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+	        recogerOperaciones = "^";
+	        textfield.setText(textfield.getText() + "^");
+	        textfield.requestFocusInWindow();
+	    }
+	});
+	frmCalculadora.getContentPane().add(botonPotencia);
+	
+	JButton raiz = new JButton(" √");
+	raiz.setBounds(107, 36, 64, 32);
+	frmCalculadora.getContentPane().add(raiz);
+	raiz.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+	        calcularRaizCuadrada();
+	    }
+	});
+	frmCalculadora.getContentPane().add(raiz);
+	
+	JButton porciento = new JButton("%");
+	porciento.setBounds(33, 37, 61, 31);
+	frmCalculadora.getContentPane().add(porciento);
+	porciento.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+	        calcularPorcentaje();
+	    }
+	});
+	frmCalculadora.getContentPane().add(porciento);
+	
+	
+	
 	}
+	//creo el metodo de elevar a la potencia
+	private double elevar(double base, double exponente) {
+	    return Math.pow(base, exponente);
+	}
+	//metodo para la raiz cuadrada
+	private void calcularRaizCuadrada() {
+	    String contenido = textfield.getText();
+	    double resultado = 0.0;
 
+	    if (!contenido.isEmpty()) {
+	        double num = Double.parseDouble(contenido);
+	        if (num >= 0) {
+	            resultado = Math.sqrt(num);
+	            textfield.setText(String.valueOf(resultado));
+	        } else {
+	            textfield.setText("Error: Número negativo");
+	        }
+	    }
+
+	    textfield.requestFocusInWindow();
+	}
+	
+	
+	//metodo para calcular el porcentaje
+	private void calcularPorcentaje() {
+	    String contenido = textfield.getText();
+	    double resultado = 0.0;
+
+	    if (!contenido.isEmpty()) {
+	        double num = Double.parseDouble(contenido);
+	        resultado = num / 100.0;
+	        textfield.setText(String.valueOf(resultado));
+	    }
+
+	    textfield.requestFocusInWindow();
+	}
+	
+	
+	//metodos para calcular
+    private void calcularResultado() {
+        String contenido = textfield.getText();
+        String operador = "";
+        double resultado = 0.0;
+
+        if (contenido.contains("+")) {
+            operador = "+";
+        } else if (contenido.contains("-")) {
+            operador = "-";
+        } else if (contenido.contains("*")) {
+            operador = "*";
+        } else if (contenido.contains("/")) {
+            operador = "/";
+        } else if (contenido.contains("^")) {
+            operador = "^";
+        }
+
+        if (!operador.isEmpty()) {
+            String[] calcularNumeros = contenido.split(Pattern.quote(operador));
+            if (calcularNumeros.length == 2) {
+                double num1 = Double.parseDouble(calcularNumeros[0]);
+                double num2 = Double.parseDouble(calcularNumeros[1]);
+
+                if (operador.equals("+")) resultado = num1 + num2;
+                else if (operador.equals("-")) resultado = num1 - num2;
+                else if (operador.equals("/")) resultado = num1 / num2;
+                else if (operador.equals("*")) resultado = num1 * num2;
+                else if (operador.equals("^")) resultado = elevar(num1, num2);
+            }
+        }
+
+        if (!operador.isEmpty()) {
+            textfield.setText(String.valueOf(resultado));
+        } else {
+            textfield.setText("");
+        }
+        textfield.requestFocusInWindow();
+    }
 }
+
+
 
 
 
